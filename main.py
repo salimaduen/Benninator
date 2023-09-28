14546ad78f8cd888d38439f96e1bc3f049776cac
Salimaduen <salimaduen@hotmail.com>
2023-09-28 13:09:47 -0400

    Updated log and total commands

--- main.py
+++ main.py
@@ -71,25 +71,32 @@ async def on_message(message):
 	if message.content.startswith("$benny_help"):
 		msg = '''
 		List of commands:
-		- $benny_log - Prints the ten most recent benny logs
-		- $benny_total - Prints the total benny deafened time
-		- $benny_code - Prints the info for the source code
+		$benny_log - Prints the ten most recent benny logs
+		$benny_total - Prints the total benny deafened time
+		$benny_code - Prints the info for the source code
 		      '''
+		await client.get_channel(channel_id).send(msg)
 
 	if message.content.startswith("$benny_log"):
 		rows = await Database().get_benny_log()
-		final_rows = ['THE THE MOST RECENT LOGS:']
-		for row in rows:
-			curr = ''.join([row[0], ' | ', f'{str(row[1]):.4}', ' SECONDS'])
-			final_rows.append(curr)
+		if row:
+			final_rows = ['THE TEN MOST RECENT LOGS:']
+			for row in rows:
+				curr = ''.join([row[0], ' | ', f'{str(row[1]):.4}', ' SECONDS'])
+				final_rows.append(curr)
 
-		f = '\n'.join(final_rows)
+			f = '\n'.join(final_rows)
+		else:
+			f = 'No logs available at this moment'
 		await client.get_channel(channel_id).send(f)
 
 	if message.content.startswith("$benny_total"):
 		t = await Database().get_total_time()
-		t, total = t[0]
-		msg = f'As of {t} <@{message.author.id}> has been deafened for a total of {total:.4} seconds'
+		if t:
+			t, total = t[0]
+			msg = f'As of {t} <@{message.author.id}> has been deafened for a total of {total:.4} seconds'
+		else:
+			msg = 'No total time available at this moment'
 		await client.get_channel(channel_id).send(msg)
 
 	if message.content.startswith("$benny_code"):
